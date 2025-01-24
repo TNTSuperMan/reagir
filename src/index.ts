f5c6d6531b6a2fa4f8d8c50a076f9446f61aae98
TNTSuperMan <tntsuperga@gmail.com>
2025-01-24 16:54:22 +0900

    エクスポートに追加

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from "./type";
 
 export { createComponent } from "./node/component";
+export { v2fnode, fragment } from "./node/fragment";
 export { seg } from "./node/seg";
 export { t } from "./node/t";
 export { useObject, usePrimitive } from "./react/use";
