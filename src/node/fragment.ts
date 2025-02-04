1a23afc340ec9604a31b0d6987819ca08ecf404b
TNTSuperMan <tntsuperga@gmail.com>
2025-02-04 21:33:04 +0900

    差分更新を止める

--- src/node/fragment.ts
+++ src/node/fragment.ts
@@ -16,27 +16,7 @@ export const fragment = (children: ()=>FNode[]): FragmentNode =>
     el.appendChild(entry);
     console.log(el)
     hook(children, fnodes=>{
-        let currentNode: ChildNode = entry;
-        const iterate = (count: number) => {
-            let current: ChildNode = entry;
-            for(let i = 0;i < count;i++)
-                if(current.nextSibling) current = current.nextSibling
-            return current;
-        }
-        let oldcount = before.length;
-        fnodes.forEach((e,i)=>{
-            const k = before.findIndex((t,j)=>i<j&&t===e.key);
-            if(k != -1){
-                el.replaceChild(currentNode, iterate(k - i));
-                oldcount--;
-            }else{
-                console.log(currentNode)
-                el.insertBefore(currentNode, e.node().node);
-            }
-        })
-        for(let i = 0;i < oldcount;i++){
-            currentNode = entry;
-            iterate(before.length + fnodes.length - i).remove();
-        }
+        before.forEach(()=>entry.nextSibling?.remove())
+        entry.after(...(fnodes.map(e=>e.node().node)))
     })
 }
