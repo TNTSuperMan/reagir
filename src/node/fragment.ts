5d1f33bb6c10567deea3f46b5d61b4980ecf27ee
TNTSuperMan <tntsuperga@gmail.com>
2025-02-04 22:32:46 +0900

    型ミス

--- src/node/fragment.ts
+++ src/node/fragment.ts
@@ -1,7 +1,7 @@
 import { hook } from "../react/hook";
 import type { FragmentNode, FreeVNode } from "../type";
 
-export const fragment = (children: ()=>(()=>FreeVNode)[]): FragmentNode =>
+export const fragment = (children: ()=>FreeVNode[]): FragmentNode =>
         (el) => {
     let before: void[] = [];
     const entry = document.createComment("");
@@ -9,7 +9,7 @@ export const fragment = (children: ()=>(()=>FreeVNode)[]): FragmentNode =>
     console.log(el)
     hook(children, fnodes=>{
         before.forEach(()=>entry.nextSibling?.remove())
-        entry.after(...(fnodes.map(e=>e()[0])))
+        entry.after(...(fnodes.map(e=>e[0])))
         before = fnodes.map(e=>{})
     })
 }
