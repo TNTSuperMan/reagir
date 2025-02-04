35dc9d7efb90f032bcfeb095effea1f1c410d998
TNTSuperMan <tntsuperga@gmail.com>
2025-02-04 22:31:40 +0900

    keyを消す

--- src/node/fragment.ts
+++ src/node/fragment.ts
@@ -1,20 +1,15 @@
 import { hook } from "../react/hook";
 import type { FragmentNode, FreeVNode } from "../type";
 
-type FNode = [unknown, ()=>FreeVNode]
-export const fnode = (key: unknown, node: ()=>FreeVNode): FNode => [
-    key, node
-]
-
-export const fragment = (children: ()=>FNode[]): FragmentNode =>
+export const fragment = (children: ()=>(()=>FreeVNode)[]): FragmentNode =>
         (el) => {
-    let before: unknown[] = [];
+    let before: void[] = [];
     const entry = document.createComment("");
     el.appendChild(entry);
     console.log(el)
     hook(children, fnodes=>{
         before.forEach(()=>entry.nextSibling?.remove())
-        entry.after(...(fnodes.map(e=>e[1]()[0])))
-        before = fnodes.map(e=>e[0])
+        entry.after(...(fnodes.map(e=>e()[0])))
+        before = fnodes.map(e=>{})
     })
 }
